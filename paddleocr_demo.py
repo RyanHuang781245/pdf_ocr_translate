b4b5ef5d2c9e1e0d858248519e56f24966e7b2b7
Ryan Huang 黃晧倫 <ryan.huang@unitedorthopedic.com>
2026-01-06 17:34:47 +0800

    paddleocr translate

---
+++ paddleocr_demo.py
@@ -0,0 +1,16 @@
+from paddleocr import PaddleOCR
+# 初始化 PaddleOCR 執行個體
+ocr = PaddleOCR(
+    use_doc_orientation_classify=False,
+    use_doc_unwarping=False,
+    use_textline_orientation=False)
+
+# 對範例圖片執行 OCR 推論
+result = ocr.predict(
+    input="UOC-PQR-16013_v2.pdf")
+    
+# 將結果視覺化並儲存為 JSON
+for res in result:
+    res.print()
+    res.save_to_img("output")
+    res.save_to_json("output")
